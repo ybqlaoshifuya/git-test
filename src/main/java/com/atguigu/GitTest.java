ae5d2d1d47c4ab4c93952c8046e5b57b799d4124
ybqlaoshifuya <116448128+ybqlaoshifuya@users.noreply.github.com>
2022-10-23 21:19:50 +0800

    poll test

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master");
         System.out.println("hot fix");
         System.out.println("github push");
+          System.out.println("poll test");
     }
 }
